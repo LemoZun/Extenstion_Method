3ee3083dbd97085a4267bd7da51b8e3e855f4806
LemoZun <marrzun@gmail.com>
2024-08-05 09:17:13 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Extension_Method
+﻿using System.Text.RegularExpressions;
+
+namespace Extension_Method
 {
     public class Program
     {
@@ -23,6 +25,9 @@
     {
         public static bool IsAllowedID(this string id)
         {
+            //정규표현식
+            //Regex regex = new Regex(id); //부분 문자열이 아니라 문자 하나를 보는거라 이거까진 굳이?
+            
             //for (int i = 0; i < id.Length; i++)
             //{
             //    if (id[i] == '!' || id[i] == '@' || id[i] == '#' || id[i] == '$' || id[i] == '%' || id[i] == '^' || id[i] == '&' || id[i] == '*')
@@ -42,8 +47,11 @@
             //string.Contains의 시간복잡도도 생각하면 그냥 반복문 돌리는게 더 빠르나?
             //포함하면 true, 포함하지 않으면 false라 true 나오자마자 결과내는건 낫긴하지만
             //결국 어느걸 하던 조건이 너무 길어져버린다
+            //스위치문으로 하면 아래로 길어지는데 
 
+            
 
+            
             
         }
     }
